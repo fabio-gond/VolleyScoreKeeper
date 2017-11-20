bbe50aff211891eed74e84ab60730f90714ea339
Fabio <fabio.gondolo@gmail.com>
2017-11-20 19:14:58 +0100

    Insert all the views

--- app/src/main/java/com/example/android/volleyscorekeeper/MainActivity.java
+++ app/src/main/java/com/example/android/volleyscorekeeper/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.android.volleyscorekeeper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FrameLayout outer = (FrameLayout)findViewById(R.id.team_score_b);
+
+
+        TextView iv = (TextView)outer.findViewById(R.id.team_score);
+        iv.setText("Ciao");
     }
 }
